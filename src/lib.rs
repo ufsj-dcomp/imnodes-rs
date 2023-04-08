6899c7f9771bf385447123f71882e076f41b52aa
Diego Augusto S. C <diego.augusto@protonmail.com>
2023-04-08 19:19:18 -0300

    Remove Id generator

--- src/lib.rs
+++ src/lib.rs
@@ -27,73 +27,14 @@ pub use scopes::*;
 // maybe wrap those (same decision as in implot-rs)
 pub use sys::{ImNodesStyle, ImVec2};
 
-/// used to generate unique identifers for elements
-pub struct IdentifierGenerator {
-    current_node: i32,
-    current_pin: i32,
-    current_link: i32,
-}
-
-impl IdentifierGenerator {
-    /// create
-    pub(crate) fn new() -> Self {
-        Self {
-            current_node: 0,
-            // input and output pins use the same pool, they must not overlap,
-            // attributes as well as far as I can see
-            current_pin: 0,
-            current_link: 0,
-        }
-    }
-
-    /// Id for a Node
-    pub fn next_node(&mut self) -> NodeId {
-        let id = self.current_node;
-        self.current_node += 1;
-        NodeId { id }
-    }
-
-    /// Id for an input pin
-    pub fn next_input_pin(&mut self) -> InputPinId {
-        let id = self.current_pin;
-        self.current_pin += 1;
-        InputPinId { id }
-    }
-
-    /// Id for an output pin
-    pub fn next_output_pin(&mut self) -> OutputPinId {
-        let id = self.current_pin;
-        self.current_pin += 1;
-        OutputPinId { id }
-    }
-
-    /// Id for an attribute in a Node
-    pub fn next_attribute(&mut self) -> AttributeId {
-        let id = self.current_pin;
-        self.current_pin += 1;
-        AttributeId { id }
-    }
-
-    /// Id for a link
-    pub fn next_link(&mut self) -> LinkId {
-        let id = self.current_link;
-        self.current_link += 1;
-        LinkId { id }
-    }
-}
-
 /// Identifier for Attributes in nodes
-///
-/// TODO document what precise uniqueness constraints do these have
 #[repr(C)]
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct AttributeId {
-    id: i32,
-}
+pub struct AttributeId(pub i32);
 
 impl Into<i32> for AttributeId {
     fn into(self) -> i32 {
-        self.id
+        self.0
     }
 }
 
@@ -119,22 +60,20 @@ pub enum CoordinateSystem {
 /// Identifier for a Node
 #[repr(C)]
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct NodeId {
-    id: i32,
-}
+pub struct NodeId(pub i32);
 
 impl NodeId {
     /// Enable or disable the ability to click and drag a specific node.
     #[doc(alias = "SetNodeDraggable")]
     pub fn set_draggable(&self, draggable: bool) -> &Self {
-        unsafe { sys::imnodes_SetNodeDraggable(self.id, draggable) };
+        unsafe { sys::imnodes_SetNodeDraggable(self.0, draggable) };
         self
     }
 
     /// EditorContextMoveToNode
     #[doc(alias = "EditorContextMoveToNode")]
     pub fn move_editor_to(&self) -> &Self {
-        unsafe { sys::imnodes_EditorContextMoveToNode(self.id) };
+        unsafe { sys::imnodes_EditorContextMoveToNode(self.0) };
         self
     }
 
@@ -142,7 +81,7 @@ impl NodeId {
     #[doc(alias = "GetNodeDimensions")]
     pub fn get_dimensions(&self) -> ImVec2 {
         let mut dimension = ImVec2 { x: 0.0, y: 0.0 };
-        unsafe { sys::imnodes_GetNodeDimensions(&mut dimension as _, self.id) };
+        unsafe { sys::imnodes_GetNodeDimensions(&mut dimension as _, self.0) };
         dimension
     }
 
@@ -156,13 +95,13 @@ impl NodeId {
         let pos = ImVec2 { x, y };
         match coordinate_sytem {
             CoordinateSystem::ScreenSpace => unsafe {
-                sys::imnodes_SetNodeScreenSpacePos(self.id, pos)
+                sys::imnodes_SetNodeScreenSpacePos(self.0, pos)
             },
             CoordinateSystem::EditorSpace => unsafe {
-                sys::imnodes_SetNodeEditorSpacePos(self.id, pos)
+                sys::imnodes_SetNodeEditorSpacePos(self.0, pos)
             },
             CoordinateSystem::GridSpace => unsafe {
-                sys::imnodes_SetNodeGridSpacePos(self.id, pos)
+                sys::imnodes_SetNodeGridSpacePos(self.0, pos)
             },
         };
         self
@@ -179,13 +118,13 @@ impl NodeId {
 
         match coordinate_sytem {
             CoordinateSystem::ScreenSpace => unsafe {
-                sys::imnodes_GetNodeScreenSpacePos(&mut pos as _, self.id)
+                sys::imnodes_GetNodeScreenSpacePos(&mut pos as _, self.0)
             },
             CoordinateSystem::EditorSpace => unsafe {
-                sys::imnodes_GetNodeEditorSpacePos(&mut pos as _, self.id)
+                sys::imnodes_GetNodeEditorSpacePos(&mut pos as _, self.0)
             },
             CoordinateSystem::GridSpace => unsafe {
-                sys::imnodes_GetNodeGridSpacePos(&mut pos as _, self.id)
+                sys::imnodes_GetNodeGridSpacePos(&mut pos as _, self.0)
             },
         };
 
@@ -195,7 +134,7 @@ impl NodeId {
 
 impl Into<i32> for NodeId {
     fn into(self) -> i32 {
-        self.id
+        self.0
     }
 }
 
@@ -203,9 +142,7 @@ impl Into<i32> for NodeId {
 ///
 /// like attribute_id in the original source
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct PinId {
-    id: i32,
-}
+pub struct PinId(pub i32);
 
 impl PinId {
     /// TODO test
@@ -232,45 +169,39 @@ impl PinId {
 
 /// Id for an input
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct InputPinId {
-    id: i32,
-}
+pub struct InputPinId(pub i32);
 
 impl Into<i32> for InputPinId {
     fn into(self) -> i32 {
-        self.id
+        self.0
     }
 }
 
 impl Into<PinId> for InputPinId {
     fn into(self) -> PinId {
-        PinId { id: self.id }
+        PinId(self.0)
     }
 }
 
 /// Id for an output
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct OutputPinId {
-    id: i32,
-}
+pub struct OutputPinId(pub i32);
 
 impl Into<i32> for OutputPinId {
     fn into(self) -> i32 {
-        self.id
+        self.0
     }
 }
 
 impl Into<PinId> for OutputPinId {
     fn into(self) -> PinId {
-        PinId { id: self.id }
+        PinId(self.0)
     }
 }
 
 /// Id for a link
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct LinkId {
-    id: i32,
-}
+pub struct LinkId(pub i32);
 
 impl LinkId {
     /// checks if the link of this LinkId got removed
@@ -282,7 +213,7 @@ impl LinkId {
 
 impl Into<i32> for LinkId {
     fn into(self) -> i32 {
-        self.id
+        self.0
     }
 }
 
@@ -304,7 +235,7 @@ impl Hoverable for OutputPinId {
     /// isPinHovered
     #[doc(alias = "IsPinHovered")]
     fn is_hovered(&self, scope: &OuterScope) -> bool {
-        Some(PinId { id: self.id }) == scope.get_hovered_pin()
+        Some(PinId(self.0)) == scope.get_hovered_pin()
     }
 }
 
@@ -312,7 +243,7 @@ impl Hoverable for InputPinId {
     /// isPinHovered
     #[doc(alias = "IsPinHovered")]
     fn is_hovered(&self, scope: &OuterScope) -> bool {
-        Some(PinId { id: self.id }) == scope.get_hovered_pin()
+        Some(PinId(self.0)) == scope.get_hovered_pin()
     }
 }
 
@@ -338,7 +269,7 @@ pub fn get_hovered_node() -> Option<NodeId> {
     let mut id: i32 = -1;
     let ok = unsafe { sys::imnodes_IsNodeHovered(&mut id as _) };
     if ok {
-        Some(NodeId { id })
+        Some(NodeId(id))
     } else {
         None
     }
